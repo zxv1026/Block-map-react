34a9645822f0fba98b1a0b0b84b3a656c2c1d758
张晓钒 <zxv970209@outlook.com>
2018-11-05 22:57:22 +0800

    feature：使用google.maps.LatLngBounds（）对象来调整地图的中心和缩放

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -4,6 +4,12 @@ import { Map , Marker , GoogleApiWrapper } from 'google-maps-react'
 export class Container extends Component {
 
 	render(){
+        const { places } = this.props
+
+        var bounds = new this.props.google.maps.LatLngBounds();
+        for (var i = 0; i < places.length; i++) {
+            bounds.extend(places[i].location);
+        }
 
 		if(!this.props.loaded){
 			return <div className="loading">Loading...</div>
@@ -16,12 +22,13 @@ export class Container extends Component {
                 initialCenter={{
                     lat: 30.2743848,
                     lng: 120.1553389
-                }}>
-                {this.props.places.map((place) =>( 
+                }}
+                bounds={bounds}>
+                {places.map((place) =>( 
 	            	<Marker	
 	            		key={place.key}
 	            		title={place.title}
-	            		position={place.location}
+                        position={place.location}
 	            	/>
 	            ))}
             </Map>
